58ee3fdaa6cbc91f05b14581e8fb01e867aacf29
Adekola Gabriel <adekolaolaonipekun@gmail.com>
2025-06-02 13:36:05 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://school-project-53km.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Routes
@@ -33,4 +39,4 @@ const startServer = async () => {
   }
 };
 
-startServer();  
+startServer();
